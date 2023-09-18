f25f0cab7b525e089bc1da366e734deba5236ab7
zainsyed28 <zainsyed664@gmail.com>
2023-09-17 20:57:46 -0700

    following along with the course

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,16 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const clickHandler = () =>{
+    console.log("Clicked!");
+  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
